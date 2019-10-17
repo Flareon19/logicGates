6f2ac3eca17ef0580b061e33245f68a90c234a71
Flareon19 <aparna.rani_iot18@gla.ac.in>
2019-10-17 19:43:43 +0530

    Two fields for two inputs of the AND gate. Two constructors of this class, first will be a no-args constructor and the second will be parametrized.

---
+++ src/gates/ANDGate.java
@@ -0,0 +1,32 @@
+package gates;
+
+public class ANDGate {
+    // Todo:
+    //  3.. Getters and Setters for the two fields of this class
+    //  4. Complete the following methods
+    private int input1;
+    private int input2;
+
+    public ANDGate() {
+        input1 = 0;
+        input2 = 0;
+    }
+
+    public ANDGate(int input1, int input2) {
+        this.input1 = input1;
+        this.input2 = input2;
+    }
+
+    private int getOutput(int input1, int input2) {
+        // write your code here
+    }
+
+    private void printOutput(int input1, int input2) {
+        // write your code here
+    }
+
+    public void printTruthTable() {
+        System.out.println("----Truth Table For AND Gate----");
+        // write your code here
+    }
+}
